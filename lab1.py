73f8371a75889e951a8308f337dd99e4ce9d9682
Yinston <n3152u@gmail.com>
2025-10-21 23:21:43 +0300

    добавлена функция создания графика

--- lab1.py
+++ lab1.py
@@ -40,11 +40,36 @@ def usor(kolvo):
             mk = ' '*(dlina_step+2*(i-12))
             for st in range(kolvo):
                 step(mk, mark, st-kolvo)
+    print()
 
 
-def func():
-    ...
+def func(s):
+    function = [i / 2 for i in range(s)]
+    graf = [[0 for i in range(s)] for j in range(s)]
+    step = round((max(function) - min(function)) / s, 2)
+    for i in range(s):
+        graf[i][0] = round(step*(s-i), 2)
+    for i in range(s):
+        for j in range(s):
+            if abs(function[j] - graf[i][0]) < step:
+                if j != 0:
+                    graf[i][j] = '!!'
+            else:
+                if j != 0:
+                    graf[i][j] = '--'
+         
+    for i in graf:
+        line = '\t' + str(float(i[0])) + '\t'
+        for j in i[1:]:
+            line += str(j)
+        print(line)
+    print('\t0\t', end='')
+    for i in range(s):
+        print(i, end=' ')
+
+
 
 
 flag()
 usor(2)
+func(10)
